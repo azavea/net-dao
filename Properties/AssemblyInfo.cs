c528234340d2e985a326c2419b005afc4c2d6b76
Justin Walgran <jwalgran@azavea.com>
2013-04-25 13:19:28 -0400

    Create nuget package
    I removed the lib directory and replaced it with package dependencies.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Azavea.Open.DAO")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The core of FastDAO, an object-relational mapper for the .NET framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Azavea.Open.DAO")]
+[assembly: AssemblyProduct("FastDAO")]
 
 // Configure log4net using the .config file
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
