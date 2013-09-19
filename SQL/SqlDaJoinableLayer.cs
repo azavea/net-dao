597853da4952194db23e650d61cf9a9da414ee8f
Matthew McFarland <mmcfarland@azavea.com>
2013-09-19 09:00:54 -0400

    Add Property Value Join Expression
    Adds the ability to add arbitrary property value join expressions
    from either the Right or Left side of the join, such as:
    ... FROM A JOIN B ON A.ID = B.ID AND B.TYPE = 'confirmed'....

--- SQL/SqlDaJoinableLayer.cs
+++ SQL/SqlDaJoinableLayer.cs
@@ -477,6 +477,23 @@ namespace Azavea.Open.DAO.SQL
                 queryToAddTo.Sql.Append(rightPrefix);
                 queryToAddTo.Sql.Append(rightMapping.AllDataColsByObjAttrs[lesser.RightProperty]);
             }
+            else if (expr is AbstractPropertyValueJoinExpression)
+            {
+                var joinExpr = (AbstractPropertyValueJoinExpression) expr;
+                
+                var prefix = leftPrefix;
+                var mapping = leftMapping;
+                if (expr is RightPropertyValueEqualJoinExpression)
+                {
+                    prefix = rightPrefix;
+                    mapping = rightMapping;
+                }
+                var dbDataType = mapping.DataColTypesByObjAttr[joinExpr.Property];
+                queryToAddTo.Sql.Append(prefix);
+                queryToAddTo.Sql.Append(mapping.AllDataColsByObjAttrs[joinExpr.Property]);
+                queryToAddTo.Sql.Append(expr.TrueOrNot() ? " = " : " <> ");
+                AppendParameter(queryToAddTo, joinExpr.Value, dbDataType);
+            }
             else
             {
                 throw new NotSupportedException("Expression type '" + expr.GetType() + "' is not supported.");
