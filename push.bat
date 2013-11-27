fe5a07a3a9ae6ef9cf1e60f19fc72216f237339b
Rick Mohr <rmohr@azavea.com>
2013-11-27 13:39:42 -0500

    Use improved pack and push scripts

--- push.bat
+++ push.bat
@@ -1,5 +1,3 @@
-echo off
-set pkg_name=%1
-echo on
-.nuget\NuGet.exe push %pkg_name%
-.nuget\Nuget.exe push %pkg_name% -s "http://nupeek.internal.azavea.com/"
+.nuget\NuGet.exe push nupkg\*.nupkg -Source http://nupeek.internal.azavea.com/
+.nuget\NuGet.exe push nupkg\*.symbols.nupkg -Source http://nupeek.internal.azavea.com/
+.nuget\NuGet.exe push nupkg\*.nupkg
