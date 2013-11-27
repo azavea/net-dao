7731bd5ccd6d59781d8623cab635bf9b8baae384
Rick Mohr <rmohr@azavea.com>
2013-11-27 15:47:25 -0500

    1.2.2 - Updates for NuGet
    * Pin log4net to version 1.2.11 (because we can't upgrade itextsharp, which uses it)
    * Scripts for packing and pushing NuGet package use their own "nupkg" folder

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
-echo off
+@echo off
 if not exist nupkg mkdir nupkg
 del /q nupkg\*
-echo on
+echo ---- Packaging code and symbols
 .nuget\NuGet.exe pack -Symbols -Prop Configuration=Release -OutputDirectory nupkg
--- push.bat
+++ push.bat
@@ -1,3 +1,29 @@
-.nuget\NuGet.exe push nupkg\*.nupkg -Source http://nupeek.internal.azavea.com/
-.nuget\NuGet.exe push nupkg\*.symbols.nupkg -Source http://nupeek.internal.azavea.com/
-.nuget\NuGet.exe push nupkg\*.nupkg
+@echo off
+
+for %%f in (nupkg\*) do (
+
+    REM Does this filename contain ".symbols.nupkg"?
+
+    echo.%%f | findstr /C:".symbols.nupkg" 1>nul
+
+    if errorlevel 1 (
+
+        echo.
+        echo ---- Pushing package to public repository
+        .nuget\NuGet.exe push %%f
+        if errorlevel 1 exit
+
+        echo.
+        echo ---- Pushing package to local repository
+        .nuget\NuGet.exe push %%f -Source http://nupeek.internal.azavea.com/
+
+    ) else (
+
+        echo.
+        echo ---- Pushing symbols to local repository
+        .nuget\NuGet.exe push %%f -Source http://nupeek.internal.azavea.com/
+
+    )
+
+)
+
